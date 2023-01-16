f02c83d1fd5096d67dc4ced1141c43e1f8eb803d
Sergey-SM2021 <sergey2003.k.96@gmail.com>
2023-01-16 21:21:14 +0300

    FEAT: Сверстал header

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="wrapper">
+    <app-header />
+  </div>
+</template>
+<script>
+import appHeader from "@/components/header.vue";
+export default {
+  components: { appHeader },
+};
+</script>
+<style lang="scss">
+@import "./_style.sass";
+
+.wrapper {
+  min-height: 100vh;
+  height: 100vh;
+  width: 100%;
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  color: $gray;
+}
+
+ul,
+li {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
